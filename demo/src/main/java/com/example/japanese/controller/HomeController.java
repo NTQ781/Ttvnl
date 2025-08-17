390a8b7d2922bbc9522550cd0d6ba2c1c7718806
NTQ781 <22t1020350@husc.edu.vn>
2025-08-18 01:02:28 +0700

    Update HomeController.java

--- demo/src/main/java/com/example/japanese/controller/HomeController.java
+++ demo/src/main/java/com/example/japanese/controller/HomeController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
+import com.example.japanese.model.User;
 
 import java.util.Arrays;
 import java.util.List;
