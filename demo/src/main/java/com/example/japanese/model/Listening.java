f266460655dae00fcd829b9fd48966c11f7f44ef
NTQ781 <22t1020350@husc.edu.vn>
2025-08-18 23:38:19 +0700

    Update Listening.java

--- demo/src/main/java/com/example/japanese/model/Listening.java
+++ demo/src/main/java/com/example/japanese/model/Listening.java
@@ -1,13 +1,19 @@
 package com.example.japanese.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Listening {
-    private String title;    // tiêu đề audio
-    private String audioUrl; // link mp3
+    private String title;
+    private String audioUrl;
+
+    public Listening(String title, String audioUrl) {
+        this.title = title;
+        this.audioUrl = audioUrl;
+    }
+
+    // Getter + Setter
+    public String getTitle() {
+        return title;
+    }
+    public String getAudioUrl() {
+        return audioUrl;
+    }
 }
