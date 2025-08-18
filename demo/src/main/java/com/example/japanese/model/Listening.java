f44f15f4925d9672c308754ead82a4bc0731b770
NTQ781 <22t1020350@husc.edu.vn>
2025-08-18 23:22:33 +0700

    Update Listening.java

--- demo/src/main/java/com/example/japanese/model/Listening.java
+++ demo/src/main/java/com/example/japanese/model/Listening.java
@@ -1,28 +1,13 @@
 package com.example.japanese.model;
 
-import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Listening {
-    private String title;
-    private String audio;
-    private String content;
-    private String level;
-    private String script;
-    private List<String> questions;
-
-    public Listening(String title, String audio, String content, String level, String script, List<String> questions) {
-        this.title = title;
-        this.audio = audio;
-        this.content = content;
-        this.level = level;
-        this.script = script;
-        this.questions = questions;
-    }
-
-    public String getTitle() { return title; }
-    public String getAudio() { return audio; }
-    public String getContent() { return content; }
-    public String getLevel() { return level; }
-    public String getScript() { return script; }
-    public List<String> getQuestions() { return questions; }
+    private String title;    // tiêu đề audio
+    private String audioUrl; // link mp3
 }
