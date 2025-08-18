37f19dccb657e9181331c068ab146d92e542127b
NTQ781 <22t1020350@husc.edu.vn>
2025-08-18 23:38:34 +0700

    Update Reading.java

--- demo/src/main/java/com/example/japanese/model/Reading.java
+++ demo/src/main/java/com/example/japanese/model/Reading.java
@@ -1,13 +1,19 @@
 package com.example.japanese.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Reading {
-    private String title;   // tiêu đề bài đọc
-    private String content; // nội dung văn bản
+    private String title;
+    private String content;
+
+    public Reading(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    // Getter + Setter
+    public String getTitle() {
+        return title;
+    }
+    public String getContent() {
+        return content;
+    }
 }
